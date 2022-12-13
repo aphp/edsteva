9917c52d31f438cab1e9dc125ea11e7b2e9b53ee
Adam <adam.remaki@aphp.fr>
2022-12-13 17:51:21 +0100

    feat: :sparkles: ConditionProbe

--- edsteva/probes/__init__.py
+++ edsteva/probes/__init__.py
@@ -1,3 +1,4 @@
 from edsteva.probes.base import BaseProbe
+from edsteva.probes.condition import ConditionProbe
 from edsteva.probes.note import NoteProbe
 from edsteva.probes.visit import VisitProbe
