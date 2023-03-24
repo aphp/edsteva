5e498bb2e0e993b7c0f7c182df972cfd67906cd2
Adam <adam.remaki@aphp.fr>
2023-03-24 10:17:24 +0100

    Hot fix

---
+++ edsteva/probes/note/viz_configs/per_visit/__init__.py
@@ -0,0 +1,5 @@
+from .estimates_densities_plot import get_estimates_densities_plot_config
+from .normalized_probe_dashboard import get_normalized_probe_dashboard_config
+from .normalized_probe_plot import get_normalized_probe_plot_config
+from .probe_dashboard import get_probe_dashboard_config
+from .probe_plot import get_probe_plot_config
