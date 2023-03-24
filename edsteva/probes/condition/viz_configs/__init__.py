8a6c30b1e2107c0af25b16b0ac721569a276533c
Adam <adam.remaki@aphp.fr>
2023-03-24 10:09:40 +0100

    Hot fix

--- edsteva/probes/condition/viz_configs/__init__.py
+++ edsteva/probes/condition/viz_configs/__init__.py
@@ -1,76 +1,49 @@
 import catalogue
 
-from edsteva.probes.condition.viz_configs.per_condition.estimates_densities_plot import (
-    get_estimates_densities_plot_config,
-)
-from edsteva.probes.condition.viz_configs.per_condition.normalized_probe_dashboard import (
-    get_normalized_probe_dashboard_config,
-)
-from edsteva.probes.condition.viz_configs.per_condition.normalized_probe_plot import (
-    get_normalized_probe_plot_config,
-)
-from edsteva.probes.condition.viz_configs.per_condition.probe_dashboard import (
-    get_probe_dashboard_config,
-)
-from edsteva.probes.condition.viz_configs.per_condition.probe_plot import (
-    get_probe_plot_config,
-)
-from edsteva.probes.condition.viz_configs.per_visit.estimates_densities_plot import (
-    get_estimates_densities_plot_config,
-)
-from edsteva.probes.condition.viz_configs.per_visit.normalized_probe_dashboard import (
-    get_normalized_probe_dashboard_config,
-)
-from edsteva.probes.condition.viz_configs.per_visit.normalized_probe_plot import (
-    get_normalized_probe_plot_config,
-)
-from edsteva.probes.condition.viz_configs.per_visit.probe_dashboard import (
-    get_probe_dashboard_config,
-)
-from edsteva.probes.condition.viz_configs.per_visit.probe_plot import (
-    get_probe_plot_config,
-)
+from edsteva.probes.condition.viz_configs import per_condition, per_visit
 
 normalized_probe_dashboard = catalogue.create(
     "edsteva.probes.condition.viz_configs", "normalized_probe_dashboard"
 )
 normalized_probe_dashboard.register(
-    "per_visit_default", func=get_normalized_probe_dashboard_config
+    "per_visit_default", func=per_visit.get_normalized_probe_dashboard_config
 )
 
 normalized_probe_dashboard.register(
-    "per_condition_default", func=get_normalized_probe_dashboard_config
+    "per_condition_default", func=per_condition.get_normalized_probe_dashboard_config
 )
 
 probe_dashboard = catalogue.create(
     "edsteva.probes.condition.viz_configs", "probe_dashboard"
 )
-probe_dashboard.register("per_visit_default", func=get_probe_dashboard_config)
-probe_dashboard.register("per_condition_default", func=get_probe_dashboard_config)
+probe_dashboard.register("per_visit_default", func=per_visit.get_probe_dashboard_config)
+probe_dashboard.register(
+    "per_condition_default", func=per_condition.get_probe_dashboard_config
+)
 
 estimates_densities_plot = catalogue.create(
     "edsteva.probes.condition.viz_configs", "estimates_densities_plot"
 )
 estimates_densities_plot.register(
-    "per_visit_default", func=get_estimates_densities_plot_config
+    "per_visit_default", func=per_visit.get_estimates_densities_plot_config
 )
 estimates_densities_plot.register(
-    "per_condition_default", func=get_estimates_densities_plot_config
+    "per_condition_default", func=per_condition.get_estimates_densities_plot_config
 )
 
 normalized_probe_plot = catalogue.create(
     "edsteva.probes.condition.viz_configs", "normalized_probe_plot"
 )
 normalized_probe_plot.register(
-    "per_visit_default", func=get_normalized_probe_plot_config
+    "per_visit_default", func=per_visit.get_normalized_probe_plot_config
 )
 normalized_probe_plot.register(
-    "per_condition_default", func=get_normalized_probe_plot_config
+    "per_condition_default", func=per_condition.get_normalized_probe_plot_config
 )
 
 probe_plot = catalogue.create("edsteva.probes.condition.viz_configs", "probe_plot")
-probe_plot.register("per_visit_default", func=get_probe_plot_config)
-probe_plot.register("per_condition_default", func=get_probe_plot_config)
+probe_plot.register("per_visit_default", func=per_visit.get_probe_plot_config)
+probe_plot.register("per_condition_default", func=per_condition.get_probe_plot_config)
 
 viz_configs = dict(
     normalized_probe_dashboard=normalized_probe_dashboard,
--- edsteva/probes/note/viz_configs/__init__.py
+++ edsteva/probes/note/viz_configs/__init__.py
@@ -1,69 +1,44 @@
 import catalogue
 
-from edsteva.probes.note.viz_configs.per_note.estimates_densities_plot import (
-    get_estimates_densities_plot_config,
-)
-from edsteva.probes.note.viz_configs.per_note.normalized_probe_dashboard import (
-    get_normalized_probe_dashboard_config,
-)
-from edsteva.probes.note.viz_configs.per_note.normalized_probe_plot import (
-    get_normalized_probe_plot_config,
-)
-from edsteva.probes.note.viz_configs.per_note.probe_dashboard import (
-    get_probe_dashboard_config,
-)
-from edsteva.probes.note.viz_configs.per_note.probe_plot import get_probe_plot_config
-from edsteva.probes.note.viz_configs.per_visit.estimates_densities_plot import (
-    get_estimates_densities_plot_config,
-)
-from edsteva.probes.note.viz_configs.per_visit.normalized_probe_dashboard import (
-    get_normalized_probe_dashboard_config,
-)
-from edsteva.probes.note.viz_configs.per_visit.normalized_probe_plot import (
-    get_normalized_probe_plot_config,
-)
-from edsteva.probes.note.viz_configs.per_visit.probe_dashboard import (
-    get_probe_dashboard_config,
-)
-from edsteva.probes.note.viz_configs.per_visit.probe_plot import get_probe_plot_config
+from edsteva.probes.note.viz_configs import per_note, per_visit
 
 normalized_probe_dashboard = catalogue.create(
     "edsteva.probes.note.viz_configs", "normalized_probe_dashboard"
 )
 normalized_probe_dashboard.register(
-    "per_visit_default", func=get_normalized_probe_dashboard_config
+    "per_visit_default", func=per_visit.get_normalized_probe_dashboard_config
 )
 normalized_probe_dashboard.register(
-    "per_note_default", func=get_normalized_probe_dashboard_config
+    "per_note_default", func=per_note.get_normalized_probe_dashboard_config
 )
 
 probe_dashboard = catalogue.create("edsteva.probes.note.viz_configs", "probe_dashboard")
-probe_dashboard.register("per_visit_default", func=get_probe_dashboard_config)
-probe_dashboard.register("per_note_default", func=get_probe_dashboard_config)
+probe_dashboard.register("per_visit_default", func=per_visit.get_probe_dashboard_config)
+probe_dashboard.register("per_note_default", func=per_note.get_probe_dashboard_config)
 
 estimates_densities_plot = catalogue.create(
     "edsteva.probes.note.viz_configs", "estimates_densities_plot"
 )
 estimates_densities_plot.register(
-    "per_visit_default", func=get_estimates_densities_plot_config
+    "per_visit_default", func=per_visit.get_estimates_densities_plot_config
 )
 estimates_densities_plot.register(
-    "per_note_default", func=get_estimates_densities_plot_config
+    "per_note_default", func=per_note.get_estimates_densities_plot_config
 )
 
 normalized_probe_plot = catalogue.create(
     "edsteva.probes.note.viz_configs", "normalized_probe_plot"
 )
 normalized_probe_plot.register(
-    "per_visit_default", func=get_normalized_probe_plot_config
+    "per_visit_default", func=per_visit.get_normalized_probe_plot_config
 )
 normalized_probe_plot.register(
-    "per_note_default", func=get_normalized_probe_plot_config
+    "per_note_default", func=per_note.get_normalized_probe_plot_config
 )
 
 probe_plot = catalogue.create("edsteva.probes.note.viz_configs", "probe_plot")
-probe_plot.register("per_visit_default", func=get_probe_plot_config)
-probe_plot.register("per_note_default", func=get_probe_plot_config)
+probe_plot.register("per_visit_default", func=per_visit.get_probe_plot_config)
+probe_plot.register("per_note_default", func=per_note.get_probe_plot_config)
 
 viz_configs = dict(
     normalized_probe_dashboard=normalized_probe_dashboard,
